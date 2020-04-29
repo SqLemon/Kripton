70f9692216fc091fe0bf8cacfb5b3fc27d27114a
damiMela <damimelamed@gmail.com>
2020-04-29 20:49:44 -0300

    nada nuevo. pura boludez

--- Codigo/kriptonV0.1.0/include/main.h
+++ Codigo/kriptonV0.1.0/include/main.h
@@ -1,6 +1,13 @@
 #include <Arduino.h>
 
-#define MOTD_A_PIN 0
-#define MOTD_B_PIN 1
-#define MOTI_A_PIN 2
-#define MOTI_B_PIN 3
+#define MOTD_DIR PB15
+#define MOTD_PWM PA8
+#define MOTI_DIR PA12
+#define MOTI_PWM PA11
+
+#define BTN1 PC14
+#define BTN2 PC15
+#define BTN3 PB1
+
+
+void motorRun(void);
--- Codigo/kriptonV0.1.0/src/main.cpp
+++ Codigo/kriptonV0.1.0/src/main.cpp
@@ -3,15 +3,41 @@
 #include "motors.h"
 
 int velA, velB;
-Motors motors(MOTD_A_PIN, MOTD_B_PIN, MOTI_A_PIN, MOTI_B_PIN);
+Motors motors(MOTD_DIR, MOTD_PWM, MOTI_DIR, MOTI_PWM);
 SimpleTimer motorExecute;
+
 
 
 void setup() {
   motors.init();
-  motorExecute.setInterval(RUN_TIME, motors.run);
+  motorExecute.setInterval(RUN_TIME, motorRun);
 }
 
 void loop() {
-  motorExecute.run();
+  int mode = menu();
+}
+
+void motorRun(){  
+  motors.run();
+}
+
+int menu(void){
+
+}
+
+int readOptions(){
+  if(digitalRead(BTN1)){
+    while(digitalRead(BTN1))
+    return 0;
+  }
+
+  if(digitalRead(BTN2)){
+    while(digitalRead(BTN2))
+    return 0;
+  }
+
+  if(digitalRead(BTN3)){
+    while(digitalRead(BTN3))
+    return 0;
+  }
 }
